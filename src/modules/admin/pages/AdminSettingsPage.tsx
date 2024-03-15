f892672f2daf50f3e6668a292effa3ea84555bc5
guinnod <210103064@stu.sdu.edu.kz>
2024-03-16 04:07:50 +0500

    feat: submenu in page

--- src/modules/admin/pages/AdminSettingsPage.tsx
+++ src/modules/admin/pages/AdminSettingsPage.tsx
@@ -1,22 +1,52 @@
-import { Button } from "antd";
-import { FC } from "react";
+import { DesktopOutlined, ShopOutlined } from "@ant-design/icons";
+import { Button, Menu, MenuProps } from "antd";
+import { FC, useState } from "react";
 import { AdminStoresTable } from "../components/store/AdminStoresTable";
+
+const items: MenuProps["items"] = [
+    {
+        label: "Stores",
+        key: "stores",
+        icon: <ShopOutlined />,
+    },
+    {
+        label: "Other page",
+        key: "other",
+        icon: <DesktopOutlined />,
+    },
+];
 
 interface AdminSettingsPageProps {}
 
 export const AdminSettingsPage: FC<AdminSettingsPageProps> = ({}) => {
+    const [current, setCurrent] = useState("stores");
+
+    const onClick: MenuProps["onClick"] = (e) => {
+        console.log("click ", e);
+        setCurrent(e.key);
+    };
     return (
-        <div>
-            <h1 className="py-4 text-2xl font-semibold">Stores</h1>
-            <Button
-                size="large"
-                type="primary"
-                className="mb-4"
-                href="/admin/settings/create-store"
-            >
-                Create a new store
-            </Button>
-            <AdminStoresTable />
+        <div className="h-full bg-white">
+            <Menu
+                items={items}
+                mode="horizontal"
+                onClick={onClick}
+                selectedKeys={[current]}
+            />
+            {current === "stores" && (
+                <div className="p-4">
+                    <h1 className="pb-4 text-2xl font-semibold">Stores</h1>
+                    <Button
+                        size="large"
+                        type="primary"
+                        className="mb-4"
+                        href="/admin/settings/create-store"
+                    >
+                        Create a new store
+                    </Button>
+                    <AdminStoresTable />
+                </div>
+            )}
         </div>
     );
 };
--- src/modules/seller/pages/SellerSettingsPage.tsx
+++ src/modules/seller/pages/SellerSettingsPage.tsx
@@ -1,13 +1,44 @@
-import { FC } from "react";
+import { DesktopOutlined, ShopOutlined } from "@ant-design/icons";
+import { Menu, MenuProps } from "antd";
+import { FC, useState } from "react";
 import { SellerStoresTable } from "../components/store/SellerStoresTable";
+
+const items: MenuProps["items"] = [
+    {
+        label: "Stores",
+        key: "stores",
+        icon: <ShopOutlined />,
+    },
+    {
+        label: "Other page",
+        key: "other",
+        icon: <DesktopOutlined />,
+    },
+];
 
 interface SellerSettingsPageProps {}
 
 export const SellerSettingsPage: FC<SellerSettingsPageProps> = ({}) => {
+    const [current, setCurrent] = useState("stores");
+
+    const onClick: MenuProps["onClick"] = (e) => {
+        console.log("click ", e);
+        setCurrent(e.key);
+    };
     return (
-        <div>
-            <h1 className="py-4 text-2xl font-semibold">Stores</h1>
-            <SellerStoresTable />
+        <div className="h-full bg-white">
+            <Menu
+                items={items}
+                mode="horizontal"
+                onClick={onClick}
+                selectedKeys={[current]}
+            />
+            {current === "stores" && (
+                <div className="p-4">
+                    <h1 className="pb-4 text-2xl font-semibold">Stores</h1>
+                    <SellerStoresTable />
+                </div>
+            )}
         </div>
     );
 };
