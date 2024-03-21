9d729d58ea1f37e90edfd760e09e86d175feff91
guinnod <210103064@stu.sdu.edu.kz>
2024-03-21 16:44:04 +0500

    feat: products with backend

--- src/modules/seller/pages/SellerProductsUploadPage.tsx
+++ src/modules/seller/pages/SellerProductsUploadPage.tsx
@@ -43,7 +43,7 @@ export const SellerProductsUploadPage: FC<
                                         );
                                     }
                                 })
-                                .catch((err) => {
+                                .catch(() => {
                                     message.error("Product upload failed");
                                 })
                                 .finally(() => {
