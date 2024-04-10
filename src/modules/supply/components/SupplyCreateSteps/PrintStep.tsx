c36041c5e24387344c95e9bba5eac02efc8dd86e
guinnod <210103064@stu.sdu.edu.kz>
2024-04-10 20:07:39 +0500

    feat: filter packs in print

--- src/modules/supply/components/SupplyCreateSteps/PrintStep.tsx
+++ src/modules/supply/components/SupplyCreateSteps/PrintStep.tsx
@@ -24,41 +24,50 @@ export const PrintStep: FC<PrintStepProps> = ({}) => {
             </h2>
             <h2 className="mb-4 text-xl font-medium">Date: {supply.date}</h2>
             <div className="grid grid-cols-2 gap-4">
-                {packs.map((pack, index) => (
-                    <PackItem key={pack.id} pack={pack} index={index} />
-                ))}
+                {packs
+                    .filter(
+                        (pack) =>
+                            pack.products.reduce(
+                                (acum, current) => acum + current.quantity,
+                                0
+                            ) > 0
+                    )
+                    .map((pack, index) => (
+                        <PackItem key={pack.id} pack={pack} index={index} />
+                    ))}
             </div>
         </div>
     );
 };
 
-const PackItem: FC<{ pack: SupplyPack; index: number }> = ({ pack }) => {
+const PackItem: FC<{ pack: SupplyPack; index: number }> = ({ pack, index }) => {
     const { data: boxes } = useGetBoxes();
+    const box = boxes?.find((box) => `${box.id}` == `${pack.box}`);
     return (
         <Card
             title={
                 <div className="flex gap-4">
-                    <h2>Box id: {pack.id}</h2>
+                    <h2>Pack {index + 1}</h2>
                 </div>
             }
         >
             <div className="flex flex-col gap-4">
                 <h3>
-                    Box type:{" "}
-                    {
-                        boxes?.find((box) => `${box.id}` == `${pack.box}`)
-                            ?.description
-                    }
+                    Box type: {box?.name}
+                    {", "}
+                    {box?.description}
                 </h3>
                 <div>
                     <h3>Products:</h3>
                     <ol>
-                        {pack.products.map((product, index) => (
-                            <li key={product.id} className="flex">
-                                {index + 1} - {product.product.name},{" "}
-                                {product.quantity} шт.
-                            </li>
-                        ))}
+                        {pack.products
+                            .filter((product) => product.quantity > 0)
+                            .map((product, index) => (
+                                <li key={product.id} className="flex">
+                                    {index + 1} - {product.product.name},{" "}
+                                    {product.quantity} шт.
+                                </li>
+                            ))}
                     </ol>
                 </div>
             </div>
