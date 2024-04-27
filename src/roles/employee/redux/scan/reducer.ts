88276e3b37c570a28f54b7243e842d0a2e2ee0a7
guinnod <210103064@stu.sdu.edu.kz>
2024-04-27 09:44:02 +0500

    fix: store state

--- src/roles/employee/redux/scan/reducer.ts
+++ src/roles/employee/redux/scan/reducer.ts
@@ -12,60 +12,63 @@ export const scanReducer = (
     state = INITIAL_STATE,
     action: actionTypes.ScanActions
 ): typeof INITIAL_STATE => {
-    try {
-        switch (action.type) {
-            case actionTypes.SET_BOX_BARCODE:
-                return {
-                    ...state,
-                    boxBarcode: action.payload,
-                };
-            case actionTypes.CREATE_SCANNING_CELL:
-                if (
-                    state.cells.find((cell) => cell.barcode === action.payload)
-                ) {
-                    return state;
-                }
-                return {
-                    ...state,
-                    cells: [
-                        ...state.cells,
-                        {
-                            barcode: action.payload,
-                            products: [],
-                        },
-                    ],
-                    currentCellBarcode: action.payload,
-                };
-            case actionTypes.ADD_PRODUCTS_TO_CELL:
-                return {
-                    ...state,
-                    cells: state.cells.map((cell) => {
-                        if (cell.barcode === action.payload.cellBarcode) {
-                            const temp = action.payload.productBarcode.filter(
-                                (barcode) => !cell.products.includes(barcode)
-                            );
-                            return {
-                                ...cell,
-                                products: [...cell.products, ...temp],
-                            };
-                        }
-                        return cell;
-                    }),
-                };
-            case actionTypes.SET_CURRENT_CELL_BARCODE:
-                return {
-                    ...state,
-                    currentCellBarcode: action.payload,
-                };
-            default:
-                return state;
-        }
-    } finally {
-        myLocalStorage?.set("scan-box-barcode", state.boxBarcode);
-        myLocalStorage?.set("scan-cells", state.cells);
-        myLocalStorage?.set(
-            "scan-current-cell-barcode",
-            state.currentCellBarcode
-        );
+    let newState = state;
+    switch (action.type) {
+        case actionTypes.SET_BOX_BARCODE:
+            newState = {
+                ...state,
+                boxBarcode: action.payload,
+            };
+            break;
+        case actionTypes.CREATE_SCANNING_CELL:
+            if (state.cells.find((cell) => cell.barcode === action.payload)) {
+                newState = state;
+                break;
+            }
+            newState = {
+                ...state,
+                cells: [
+                    ...state.cells,
+                    {
+                        barcode: action.payload,
+                        products: [],
+                    },
+                ],
+                currentCellBarcode: action.payload,
+            };
+            break;
+        case actionTypes.ADD_PRODUCTS_TO_CELL:
+            newState = {
+                ...state,
+                cells: state.cells.map((cell) => {
+                    if (cell.barcode === action.payload.cellBarcode) {
+                        const temp = action.payload.productBarcode.filter(
+                            (barcode) => !cell.products.includes(barcode)
+                        );
+                        return {
+                            ...cell,
+                            products: [...cell.products, ...temp],
+                        };
+                    }
+                    return cell;
+                }),
+            };
+            break;
+        case actionTypes.SET_CURRENT_CELL_BARCODE:
+            newState = {
+                ...state,
+                currentCellBarcode: action.payload,
+            };
+            break;
+        default:
+            newState = state;
     }
+    saveState(newState);
+    return newState;
 };
+
+function saveState(state: actionTypes.ScanState) {
+    myLocalStorage?.set("scan-box-barcode", state.boxBarcode);
+    myLocalStorage?.set("scan-cells", state.cells);
+    myLocalStorage?.set("scan-current-cell-barcode", state.currentCellBarcode);
+}
