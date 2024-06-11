0209e270b4bb1e2683f72a5aa1a25a4ccfe08905
Yerlan Jumabayev <thevarp18@gmail.com>
2024-06-11 15:52:42 +0500

    fix: admin menu

--- src/roles/admin/pages/AdminSettingsPage.tsx
+++ src/roles/admin/pages/AdminSettingsPage.tsx
@@ -14,7 +14,7 @@ import { useSearchParams } from "react-router-dom";
 
 const items: MenuProps["items"] = [
     {
-        label: "Магазины",
+        label: "Склады",
         key: "stores",
         icon: <ShopOutlined />,
     },
