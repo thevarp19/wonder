46186a59085dc5d519b169c2a3b03827b91d3cbd
Yerlan Jumabayev <thevarp18@gmail.com>
2024-06-21 20:58:42 +0500

    fix: loading image

--- src/roles/admin/pages/AdminLoginPage.tsx
+++ src/roles/admin/pages/AdminLoginPage.tsx
@@ -4,13 +4,14 @@ import { LoginForm } from "@/modules/auth/components/LoginForm";
 import { LoginResponse } from "@/modules/auth/types";
 import { useAppDispatch } from "@/redux/utils";
 import { adminLoginSuccess } from "@/roles/admin/redux/auth/actions";
-import { FC } from "react";
+import { FC, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 interface AdminLoginPageProps {}
 
 export const AdminLoginPage: FC<AdminLoginPageProps> = ({}) => {
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
     const navigateToHome = () => navigate("/admin");
     const dispatch = useAppDispatch();
 
@@ -22,13 +23,18 @@ export const AdminLoginPage: FC<AdminLoginPageProps> = ({}) => {
         <div className="flex flex-col items-center h-screen bg-[#F7F9FB]">
             <HeaderLogo />
             <div className="flex w-full gap-10">
-                <div className="w-full">
+                <div className="relative w-full">
                     <img
                         loading="lazy"
+                        onLoadStart={() => setLoading(true)}
+                        onLoad={() => setLoading(false)}
                         className="object-cover object-center w-full h-[calc(100vh-70px)]"
                         src={loginAdminBanner}
                         alt="Banner"
                     />
+                    {loading && (
+                        <div className="absolute top-0 left-0 z-10 w-full h-full bg-gray-500 animate-pulse" />
+                    )}
                 </div>
                 <div className="flex items-center justify-center w-full">
                     <div className="flex flex-col min-w-[450px]">
--- src/roles/seller/pages/SellerLoginPage.tsx
+++ src/roles/seller/pages/SellerLoginPage.tsx
@@ -5,13 +5,14 @@ import { LoginResponse } from "@/modules/auth/types";
 import { useAppDispatch } from "@/redux/utils";
 import { sellerLoginSuccess } from "@/roles/seller/redux/auth/actions";
 
-import { FC } from "react";
+import { FC, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 
 interface SellerLoginPageProps {}
 
 export const SellerLoginPage: FC<SellerLoginPageProps> = () => {
     const navigate = useNavigate();
+    const [loading, setLoading] = useState(false);
     const navigateToHome = () => navigate("/seller");
     const dispatch = useAppDispatch();
     const success = (data: LoginResponse) => {
@@ -22,13 +23,18 @@ export const SellerLoginPage: FC<SellerLoginPageProps> = () => {
         <div className="flex flex-col items-center h-screen bg-[#F7F9FB]">
             <HeaderLogo />
             <div className="flex w-full gap-10">
-                <div className="w-full">
+                <div className="relative w-full">
                     <img
                         loading="lazy"
+                        onLoadStart={() => setLoading(true)}
+                        onLoad={() => setLoading(false)}
                         className="object-cover object-center w-full h-[calc(100vh-70px)]"
                         src={loginSellerBanner}
                         alt="Banner"
                     />
+                    {loading && (
+                        <div className="absolute top-0 left-0 z-10 w-full h-full bg-gray-500 animate-pulse" />
+                    )}
                 </div>
                 <div className="flex items-center justify-center w-full">
                     <div className="flex flex-col min-w-[450px]">
