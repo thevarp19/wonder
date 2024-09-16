359cb5163c9f0351e7984cffbc8547d643620b4f
Yerlan Jumabayev <thevarp18@gmail.com>
2024-09-16 17:47:18 +0500

    fix: seller supply fix

--- src/modules/supply/components/SuppliesTable/SellerSuppliesTable.tsx
+++ src/modules/supply/components/SuppliesTable/SellerSuppliesTable.tsx
@@ -1,33 +1,16 @@
 import { CustomTable } from "@/components/ui/CustomTable";
 import { padNumbers } from "@/utils/shared.util";
 import { DownloadOutlined } from "@ant-design/icons";
-import { Button, TableColumnsType, Tag } from "antd";
+import { Button, Select, TableColumnsType } from "antd";
 import { FC } from "react";
 import { Link } from "react-router-dom";
 import { useGetSellerSupplies } from "../../queries";
-import { GetSellerSupply, SupplyState } from "../../types";
+import { GetSellerSupply } from "../../types";
 // import { SupplyPDFReportModal } from "../SupplyReportPDF/SupplyPDFReportModal";
 
 // interface Supply {}
 
 interface SellerSuppliesTableProps {}
-
-function getColor(status: SupplyState) {
-    switch (status) {
-        case "START":
-            return "blue";
-        case "ACCEPTED":
-            return "green";
-        case "IN_PROGRESS":
-            return "orange";
-        case "IN_DELIVERY":
-            return "purple";
-        case "REJECTED":
-            return "red";
-        default:
-            return "blue";
-    }
-}
 
 export const SellerSuppliesTable: FC<SellerSuppliesTableProps> = ({}) => {
     const { data, isPending } = useGetSellerSupplies();
@@ -60,7 +43,58 @@ export const SellerSuppliesTable: FC<SellerSuppliesTableProps> = ({}) => {
             title: "Статус",
             dataIndex: "status",
             render: (status) => {
-                return <Tag color={getColor(status)}>{status}</Tag>;
+                return (
+                    <Select
+                        defaultValue={status}
+                        onChange={(_, option) => console.log("status", option)}
+                        options={[
+                            { value: null, label: "Не выбрано" },
+                            { value: 1, label: "Принято" },
+                            { value: 2, label: "Брак" },
+                            { value: 3, label: "Отправлено" },
+                            { value: 4, label: "В процессе" },
+                            { value: 5, label: "Ошибка" },
+                            { value: 6, label: "Принято" },
+                        ]}
+                    ></Select>
+                );
+            },
+        },
+        {
+            title: "Заявлено",
+            dataIndex: "declared_products",
+        },
+        {
+            title: "Принято",
+            dataIndex: "accepted_products",
+        },
+
+        {
+            title: "Ошибка",
+            dataIndex: "fallacy_products",
+        },
+        {
+            title: "Брак",
+            dataIndex: "defective_products",
+        },
+
+        {
+            title: "Доверенность",
+            render: (_, record) => {
+                return (
+                    <Link target="_blank" to={record.report_a4}>
+                        <Button
+                            danger
+                            loading={false}
+                            icon={
+                                <DownloadOutlined
+                                    color="#ef7214"
+                                    style={{ color: "#ef7214" }}
+                                />
+                            }
+                        ></Button>
+                    </Link>
+                );
             },
         },
         {
